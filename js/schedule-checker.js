4ee9c6647e8eb4753200075684053e5ca507fbda
Kevin Peterson <kevin.peterson@mayo.edu>
2014-02-07 15:27:44 -0600

    Started verifier.

---
+++ js/schedule-checker.js
@@ -0,0 +1,27 @@
+/*
+
+ Check your generated schedule to confirm that every job for every task
+ executes at its required period between its required offset and deadline constraints,
+ that the interval reserved in the schedule for
+ every job is large enough for its WCET, and that the
+ execution of no two jobs ever overlap
+
+*/
+
+function check_schedule(workload){
+    var test_results = {};
+
+
+
+
+
+    return test_results;
+}
+
+function check_each_task_executes_in_period(){
+
+}
+
+function check_overlap(){
+
+}
